8483172e1f44a3569ad37db3e9da340bfe33f771
7biok <roofsdream@gmail.com>
2019-07-05 22:14:27 +0500

    first deploy

---
+++ Bot.py
@@ -0,0 +1,28 @@
+import telebot
+import diction
+
+bot = telebot.TeleBot('775875615:AAH_vuupN8bXJcy8M-zElD5Kiz4iCh2DsmA')
+keyboard1 = telebot.types.ReplyKeyboardMarkup()
+keyboard1.row('Привет', 'Пока')
+
+
+
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Привет,' + reply_markup, reply_markup=keyboard1)
+
+@bot.message_handler(content_types=['text'])
+def send_text(message):
+    if message.text.lower() == diction:
+        bot.send_message(message.chat.id, 'Привет, мой создатель')
+    elif message.text.lower() == 'пока':
+        bot.send_message(message.chat.id, 'Прощай, создатель')
+    elif message.text.lower() == 'я тебя люблю':
+        bot.send_sticker(message.chat.id, 'CAADAgADZgkAAnlc4gmfCor5YbYYRAI')
+
+@bot.message_handler(content_types=['sticker'])
+def sticker_id(message):
+    print(message)
+
+bot.polling()
